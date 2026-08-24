aaad1123473f3c0969c852ebe22be7598241ca4c
CoastalPOS3157 <cposremote@gmail.com>
2026-08-24 16:38:39 -0500

    Update menu

--- menu-data.js
+++ menu-data.js
@@ -23,7 +23,7 @@ window.MENU_DATA = {
       "items": [
         {
           "id": "chips-salsa",
-          "name": "Chips & Salsa true",
+          "name": "TESTING123",
           "description": "House-made tortilla chips with fresh salsa.",
           "tags": [
             "vegetarian"
